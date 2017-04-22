bedaaf3f882ec4465b4bbebd5076afdfd21119ae
Erik Skoglund <skoggy@Eriks-MBP.lan>
2017-04-22 09:33:00 +0200

    added fx and earth

---
+++ Assets/_Project/Scripts/Generic/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour 
+{
+
+	public Vector3 Axis;
+	public float Speed = 1f;
+	public Space Space = Space.World;
+
+	void Update () 
+	{
+		transform.Rotate(Axis * Speed * Time.deltaTime, Space);
+	}
+}
